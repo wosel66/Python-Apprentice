df9db9739592eb2cc1510222a5be5c31a2779899
wosel66 <wesjwurst@gmail.com>
2024-09-24 22:24:08 +0000

    9/24/24

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -15,14 +15,14 @@ turtle.setup(width=600, height=600)     # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
 
-tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+tina.shape('arrow')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Make the turtle move as fast, but not too fast.
 
 tina.pencolor('blue')                   # Set the pen color to blue
 tina.forward(150)                       # Move tina forward by the forward distance
 tina.left(90)                           # Turn tina left by the left turn
 
-tina.pencolor('red')                    # Set the pen color to red
+tina.pencolor('green')                    # Set the pen color to red
 tina.forward(150)                       # Continuie the last two steps three more times
 tina.left(90)                           # to draw a square
 
@@ -38,14 +38,14 @@ tina.penup()                            # Lift the pen up so we can move tina wi
 tina.forward(20)                        # Move tina forward by 20
 tina.left(90)                           # Turn tina left by 90 degrees
 tina.forward(20)                        # Move tina forward by 20
-tina.write("Why, hello there!")         # Write the message "Why, hello there!"
+tina.write("Blake has a gyatt")         # Write the message "Why, hello there!"
 tina.backward(20)                       # Move tina backward by 20
 
 tina.goto(-50,0)
 tina.pendown()
-tina.color('red')                       # Set the color of tina to red
+tina.color('tan')                       # Set the color of tina to red
 tina.begin_fill()
-tina.circle(101, steps=50)
+tina.circle(300, steps=150)
 tina.end_fill()
 
 turtle.exitonclick()                    # Close the window when we click on it
