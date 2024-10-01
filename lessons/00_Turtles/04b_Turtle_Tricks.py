3289e8ffc43a99bf2ae9ae2ad136a74b948734d9
wosel66 <wesjwurst@gmail.com>
2024-10-01 22:46:37 +0000

    Yeeeeehhhhaawwww

--- lessons/00_Turtles/04b_Turtle_Tricks.py
+++ lessons/00_Turtles/04b_Turtle_Tricks.py
@@ -11,12 +11,20 @@ to see how to use the turtle commands.
 # These lines are needed in most turtle programs
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup (width=600, height=600)    # Set the size of the window
-tina = turtle.Turtle()                  # Create a turtle named tina
+lw = turtle.Turtle()                  # Create a turtle named tina
 
 # Use tina.forward() and tina.left() to draw a pentagon
 # Make each side of the pentagon a different color with 
 # tina.pencolor()
 
+lw.pencolor('orange')
+lw.forward(207)
+lw.left(135)
+lw.forward(75)
+lw.left(45)
+lw.forward(100)
+lw.left(45)
+lw.forward(100)
 
 ... # Your code here
 
--- lessons/00_Turtles/04c_Turtle_Tricks.py
+++ lessons/00_Turtles/04c_Turtle_Tricks.py
@@ -15,6 +15,20 @@ import turtle                           # Tell Python we want to work with the t
 turtle.setup (width=600, height=600)    # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
 
+tina.pendown()
+tina.color('orange')
+tina.begin_fill()
+tina.circle(95, steps=150 )
+tina.end_fill()
+tina.penup()
+
+tina.
+tina.pendown()
+tina.color('blue')
+tina.begin_fill()
+tina.circle(20, steps=75)
+tina.end_fill()
+tina.circle(20)
 # Use tina.circle() to draw a circle, and tina.goto() to move tina to a new location
 # Use tina.begin_fill(), tina.end_fill(), and tina.fillcolor() to fill in the shapes
 
